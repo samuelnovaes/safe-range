cdc9aabf116ee947a69133fc45a4758d8113d24d
Samuel Novaes <samuel.novaes.96@gmail.com>
2018-09-21 02:03:03 -0300

    update

---
+++ README.md
@@ -0,0 +1,46 @@
+# Safe Range
+
+Generate safe range of numbers in JavaScript
+
+# Install
+
+## Browser
+
+```html
+<script src="..."></script>
+<script>
+	console.log(range(5)) //[1, 2, 3, 4, 5]
+	console.log(range(2, 5)) //[2, 3, 4, 5]
+	console.log(range(2, 8, 2)) //[2, 4, 6, 8]
+</script>
+```
+
+# NodeJS
+
+`npm install safe-range`
+
+```javascript
+const range = require('safe-range')
+
+console.log(range(5)) //[1, 2, 3, 4, 5]
+console.log(range(2, 5)) //[2, 3, 4, 5]
+console.log(range(2, 8, 2)) //[2, 4, 6, 8]
+```
+
+# Why is it safe?
+
+Exemple: `range(0, 1, 0.1)`
+
+safe-range	| Other libraries
+---			| ---
+0			| 0
+0.1			| 0.1
+0.2			| 0.2
+0.3			| 0.30000000000000004
+0.4			| 0.4
+0.5			| 0.5
+0.6			| 0.6
+0.7			| 0.7
+0.8			| 0.7999999999999999
+0.9			| 0.8999999999999999
+1			| 0.9999999999999999
---
+++ safe-range.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function range(start, end, step) {
+	if (end === undefined) {
+		end = start;
+		start = 0;
+	}
+	if (step === undefined) {
+		step = 1;
+	}
+	if (isNaN(start)) {
+		throw new Error('Start argument must be numeric');
+	}
+	if (isNaN(end)) {
+		throw new Error('End argument must be numeric');
+	}
+	if (isNaN(step)) {
+		throw new Error('Step argument must be numeric');
+	}
+	if (step === 0) {
+		throw new Error('Step argument must not be zero');
+	}
+	var base = Math.pow(10, step.toString().replace(/[\.-]/g, '').length - 1);
+	var arr = [];
+	start *= base;
+	end *= base;
+	step *= base;
+	if (start < end && step < 0 || start > end && step > 0) {
+		return arr;
+	}
+	for (var i = start; !(start < end && i > end || start > end && i < end); i += step) {
+		arr.push(i / base);
+	}
+	return arr;
+}
+if (typeof module != 'undefined') {
+	module.exports = range;
+}
